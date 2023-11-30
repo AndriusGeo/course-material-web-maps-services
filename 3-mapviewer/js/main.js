f7e113fc820e54c13b24e81234f43f1cceeaa397
andriusgeo <andriub@gmail.com>
2023-11-30 20:56:09 +0200

    add wms services

--- 3-mapviewer/js/main.js
+++ 3-mapviewer/js/main.js
@@ -12,3 +12,47 @@ map.addControl(new maplibregl.NavigationControl());
 function changeMapStyle(styleType) {
   map.setStyle("map_styles/" + styleType + ".openmap.style.json");
 }
+
+map.on("load", () => {
+  map.addSource("stvk-source", {
+    type: "raster",
+    tiles: [
+      "https://www.geoportal.lt/mapproxy/vstt_stk?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=nac_parkai,np_draustiniai,reg_parkai",
+    ],
+    tileSize: 256,
+  });
+  map.addLayer({
+    id: "stvk-parkai",
+    type: "raster",
+    source: "stvk-source",
+    paint: {},
+  });
+
+  map.addSource("uetk-source", {
+    type: "raster",
+    tiles: [
+      "https://gis.biip.lt/qgisserver/uetk_public?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=hidroelektrines",
+    ],
+    tileSize: 256,
+  });
+  map.addLayer({
+    id: "uetk-hidroelektrines",
+    type: "raster",
+    source: "uetk-source",
+    paint: {},
+  });
+
+  map.addSource("demo-source", {
+    type: "raster",
+    tiles: [
+      "http://127.0.0.1:8010/ogc/demo_service?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=gyv_stat_2021",
+    ],
+    tileSize: 256,
+  });
+  map.addLayer({
+    id: "demo-layer",
+    type: "raster",
+    source: "demo-source",
+    paint: {},
+  });
+});
